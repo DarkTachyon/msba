713a645f341e0e9928314864fdfea75d1043a0d7
DarkTachyon <varmavijayp@gmail.com>
2021-08-18 15:12:20 -0400

    Commiting code

--- test_file.R
+++ test_file.R
@@ -1,1 +1,18 @@
-x <- 1:10
+library(ggplot2)
+library(car)
+
+ggplot(data = mtcars, 
+       mapping = aes(x = disp, y = mpg)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+
+st_joe_land <- read.delim(file = "stJoeLand.csv", sep = ",")
+
+st_joe_land <- read.csv(file = "stJoeLand.csv")
+
+st_joe_land_readr <- readr::read_csv(file = "stJoeLand.csv")
+
+st_joe_land_datatable <- data.table::fread(input = "stJoeLand.csv")
+
+names(st_joe_land_datatable)[names(st_joe_land_datatable) == "Zip Code"] <- "Zip_Code"
